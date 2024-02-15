1df70234180a3ee73530e9f08e83476a43c6b578
Arnaud-Kastner <148237500+Arnaud-Kastner@users.noreply.github.com>
2024-02-15 22:46:58 +0100

    Create add_capteur.php

---
+++ Website/add_capteur.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang=fr>
+<head>
+    <meta charset="utf-8">
+    <title> Add Capteur </title>
+    <link rel="stylesheet" href="creation.css">
+</head>
+
+<body>
+
+<div>
+    <p>
+    <?php
+    $mySQLclient = new PDO (
+        'mysql:host=localhost;dbname=mysql;charset=utf8',
+        'root',
+        ''
+        );
+        // Check connection
+        if (!$mySQLclient) {
+            die("Échec de la connexion : ");
+        }
+       /* else {
+            echo "Connexion Success";
+        } */
+
+    if(isset($_POST['create'])){
+        $latitude = $_POST['latitude'];
+        $longitude = $_POST['longitude'];
+        $temperature = $_POST['temperature'];
+
+        echo "lat :" . $latitude;
+        echo "long : " . $longitude;
+        echo "temp : " . $temperature;
+
+        $insert = "INSERT INTO capteurs (Latitude, Longitude, Temperature) VALUES (?, ?, ?)";
+
+        $stmt = $mySQLclient -> prepare ($insert);
+        $stmt->execute([$latitude, $longitude, $temperature]); 
+    }
+    ?>
+    </p>
+</div>
+
+<div>
+    <form action="add_capteur.php" method="post">
+        <div class="container">
+            <h1> Ajout d'un capteur </h1>
+            <p> Veuillez rentrer les informations de votre capteur </p>
+            <br>
+            <br>
+            <label for="latitude"><b> Latitude :</b></label>
+            <input type="text" name="latitude" required>
+            <br>
+            <br>           
+            <label for="longitude"><b>Longitude :</b></label>
+            <input type="text" name ="longitude" required>
+            <br>
+            <br>
+            <label for="latitude"><b>Température :</b></label>
+            <input type="text" name ="temperature" required>
+            <br>
+            <br>
+            <input type="submit" name="create" value="Ajouter votre capteur">
+
+        </div>
+    </form>
+</div>
+
+</body>
