7d264704a4e96ba19b3a89911d727aa1e496cbb0
Arnaud-Kastner <148237500+Arnaud-Kastner@users.noreply.github.com>
2024-02-19 09:39:01 +0100

    Update registration.php
    adding comments

--- Website/registration.php
+++ Website/registration.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang=fr>
+<html lang="fr">
 <head>
     <meta charset="utf-8">
     <title> User Registration </title>
@@ -11,6 +11,8 @@
 <div>
     <p>
     <?php
+    // PHP code starts here
+    // Creating a PDO instance for MySQL connection
     $mySQLclient = new PDO (
         'mysql:host=localhost;dbname=mysql;charset=utf8',
         'root',
@@ -21,45 +23,38 @@
       die("Échec de la connexion : ");
     }
 
+    // Check if form is submitted
     if(isset($_POST['create'])){ 
+        // Retrieve username, email, and password from the form
         $identifiant = $_POST['identifiant'];
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        /*echo "Identifiant :" . $identifiant;
-        echo "Email : " . $email;
-        echo "Mot de passe : " . $password;*/
-        //echo "ICCCCCIIIIIIII3333333333333333333I" ;
+        // Prepare SQL statement to insert data into the database
         $insert = "INSERT INTO comptes (Identifiant, Email, Password) VALUES (?,?,?);";
-        //echo "ICCCCCIIIIIIIII"  . $insert;
         $stmt = $mySQLclient -> prepare ($insert);
+        // Execute the prepared statement with the provided data
         $stmt -> execute([$identifiant, $email,$password]);
     }
 
-
+    // Fetch data from the database
     $sth = $mySQLclient -> query('SELECT * FROM comptes ;');
     if (!$sth){
         die('Echec');
     }
-
-   /*
-    if (PDO::query($mySQLclient, $sql)) {
-        echo "Nouveau enregistrement créé avec succès";
-    } */
-    
-
-
     ?>
     </p>
 </div>
 
 <div>
+    <!-- Form for user registration -->
     <form action="registration.php" method="post">
         <div class="container">
             <h1> Création de compte </h1>
-            <p> Compléter les informations suivante pour créer votre compte </p>
+            <p> Compléter les informations suivantes pour créer votre compte </p>
             <br>
             <br>
+            <!-- Input fields for username, email, and password -->
             <label for="identifiant"><b> Identifiant :</b></label>
             <input type="text" name="identifiant" required>
             <br>
@@ -72,6 +67,7 @@
             <input type="password" name ="password" required>
             <br>
             <br>
+            <!-- Submit button -->
             <input type="submit" name="create" value="Création du compte">
 
         </div>
